d142cd0c6cbf3a4b7985b9ba71303b545853b5f0
Benoit DESROCHERS <ben.ensta@gmail.com>
2014-12-28 13:17:06 +0100

    add verbose test option

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -x
 cd ${0%%$(basename $0)}
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=./ -DCMAKE_BUILD_TYPE=DEBUG .. && make && make install
+cmake -DCMAKE_INSTALL_PREFIX=./ -DCMAKE_BUILD_TYPE=DEBUG .. && make && make install && make test ARGS="-V"
 cd ./tests/ && pwd && ./test_Interval.py && cd ../

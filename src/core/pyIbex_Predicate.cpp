412fc4bbe7de50a4093962a8c6de53242704b1a3
Benoit DESROCHERS <ben.ensta@gmail.com>
2015-09-26 10:07:44 +0200

    Bind predicate

--- src/core/pyIbex_Predicate.cpp
+++ src/core/pyIbex_Predicate.cpp
@@ -18,25 +18,47 @@
 #include <boost/python.hpp>
 using namespace boost;
 using namespace boost::python;
-using namespace ibex;
 namespace py = boost::python;
+using namespace ibex;
 
 
 
 void export_Predicate(){
 
         // Export comparaison constant
-    enum_<BoolInterval>("BoolInterval")
+    enum_<IBOOLEAN>("IBOOLEAN")
         .value( "NO",   ibex::NO)
         .value( "MAYBE",  ibex::MAYBE)
         .value( "YES",   ibex::YES)
+        .value( "EMPTY",  ibex::EMPTY)
         .export_values()
         ;
-    def<BoolInterval( const BoolInterval&, const BoolInterval&)>("IBooleanAnd", operator&);
-    def<BoolInterval( const BoolInterval&, const BoolInterval&)>("IBooleanOr", operator|);
-    def<BoolInterval( BoolInterval&, const BoolInterval&)>("IBooleaniAnd", operator&=);
-    def<BoolInterval( BoolInterval&, const BoolInterval&)>("IBooleaniOr", operator|=);
-    def<BoolInterval( const BoolInterval&)>("IBooleanNeg", operator!);
+    class_<BoolInterval, boost::shared_ptr<ibex::BoolInterval>  >("BoolInterval", init<>())
+            .def(init<bool>())
+            .def(init<IBOOLEAN>() )
+            .def(init<BoolInterval>())
+            .def(self & self)
+            .def(self | self)
+            .def(self &= self)
+            .def(self |= self)
+            .def(self == self)
+            .def(self != self)
+            ;
+
+    def("Inter",     &ibex::Inter);
+    def("Not",   &ibex::Not);
+    def("Union",     &ibex::Union);
+    def("And",   &ibex::And);
+    def("Or",    &ibex::Or);
+    def("leq",   &ibex::leq);
+    def("geq",   &ibex::geq);
+    def("Restrict",  &ibex::Restrict);
+    def("Xor",   &ibex::Xor);
+    // def<BoolInterval( const BoolInterval&, const BoolInterval&)>("IBooleanAnd", operator&);
+    // def<BoolInterval( const BoolInterval&, const BoolInterval&)>("IBooleanOr", operator|);
+    // def<BoolInterval( BoolInterval&, const BoolInterval&)>("IBooleaniAnd", operator&=);
+    // def<BoolInterval( BoolInterval&, const BoolInterval&)>("IBooleaniOr", operator|=);
+    // def<BoolInterval( const BoolInterval&)>("IBooleanNeg", operator!);
 
     
 

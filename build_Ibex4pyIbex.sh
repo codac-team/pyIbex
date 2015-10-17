6faaa39010f0b8116fd74fd9edd4b6078c4bfcec
Benoit DESROCHERS <ben.ensta@gmail.com>
2015-10-17 09:17:10 +0200

    [fix] Travis config file

---
+++ build_Ibex4pyIbex.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+# check to see if protobuf folder is empty
+if [ ! -d "$HOME/protobuf/lib" ]; then
+	git clone https://github.com/benEnsta/ibex-lib.git
+	cd ibex-lib
+	git checkout pyIbex_version
+	mkdir build
+	cd build
+	cmake -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=${HOME}/ibex ../
+	make -j2
+	make install
+else
+  echo 'Using cached directory.';
+fi

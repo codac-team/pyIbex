0249331fe35a367cda92fec131b5d625ffc87502
Benoit DESROCHERS <ben.ensta@gmail.com>
2016-07-19 16:27:16 +0200

    [pre_v1.5] add polar and geometry module (check if avaiable)

---
+++ pyibex/geometry/__init__.py
@@ -0,0 +1,5 @@
+try:
+  from pyibex_geometry import *
+except ImportError:
+  print("pyibex_geometry module not found (need to be installed).")
+  print("It can be installed with \`pip install pyibex_geometry\` ")

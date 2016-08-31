ee68ba9c78d26c6f0c9cf0c4a1de039f1e70ce79
Benoit DESROCHERS <ben.ensta@gmail.com>
2016-08-31 12:32:05 +0200

    [master] add reference to module thickset and tube

---
+++ pyibex/thickset/__init__.py
@@ -0,0 +1,5 @@
+try:
+  from pyibex_thickset import *
+except ImportError:
+  print("[pyibex_thickset] : module not found (need to be installed).")
+  print("It can be installed with conda install -c benensta pyibex_thickset")

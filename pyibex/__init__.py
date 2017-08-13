8e663f96b15675aa7c163cd2c8164311dc8c8640
Benoit DESROCHERS <ben.ensta@gmail.com>
2017-08-13 22:36:09 +0200

    [wip] Minkow and SepPaving

--- pyibex/__init__.py
+++ pyibex/__init__.py
@@ -1,5 +1,7 @@
 from .pyibex import *
 from .pysivia import pySIVIA
+from .pySepPaving import pySepPaving
 from .version import __version__
+from .minkowsky_ops import SepSum, SepDiff
 
 # try to import geometry module

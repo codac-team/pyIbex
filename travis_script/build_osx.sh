3b3d85df2af60f03c2d82fc9c452d9e40a7d7cd2
benEnsta <ben.ensta@gmail.com>
2019-02-17 16:23:23 +0100

    [CI] fix upload filenames and try xcode8.2

--- travis_script/build_osx.sh
+++ travis_script/build_osx.sh
@@ -6,7 +6,7 @@ for PY_VERSION in "2.7.14" "3.4.9" "3.5.6" "3.6.8" "3.7.2"; do
   echo "BUILD FOR PYIBEX FOR PYTHON ${PY_VERSION}";
   mkdir -p build$PY_VERSION;
   cd build$PY_VERSION;
-  export CXX=clang++ CC=clang;
+  # export CXX=clang++ CC=clang;
   # if [ $PY_VERSION != "system" ]; then
   pyenv install -s $PY_VERSION;
   # fi;

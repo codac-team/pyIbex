4f38051a2630704395905db47a3ff01c2654f21b
benEnsta <ben.ensta@gmail.com>
2020-06-02 15:26:01 +0200

    [docker] use custom image

--- scripts/build_Ibex4pyIbex.sh
+++ scripts/build_Ibex4pyIbex.sh
@@ -3,7 +3,7 @@
 set -x
 # check to see if protobuf folder is empty
 if [ ! -e "$HOME/ibex/lib/libibex.a" ]; then
-	git clone -b develop https://github.com/ibex-lib/ibex-lib.git
+	git clone -b develop https://github.com/ibex-team/ibex-lib.git
 	cd ibex-lib
 	mkdir build && cd build
 	cmake -DCMAKE_INSTALL_PREFIX=${HOME}/ibex cmake -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" ..
